5c17257a56318c2981a1ea354c4fee5cee5657f5
James Ellenstein <59277482+JamesEllenstein@users.noreply.github.com>
2023-05-22 01:01:40 +0000

    WIP please help Courseform updates

--- frontend/src/main/components/Courses/CourseForm.js
+++ frontend/src/main/components/Courses/CourseForm.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import { Button, Form } from 'react-bootstrap';
 import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom';
-
-function CourseForm({ initialCourse, submitAction, buttonLabel = "Create" }) {
-
-    const navigate = useNavigate();
-    
+import { useNavigate } from 'react-router-dom'
+function CourseForm({ initialCourse, submitAction, buttonLabel="Create" }) {
     // Stryker disable all
     const {
         register,
@@ -16,13 +12,10 @@ function CourseForm({ initialCourse, submitAction, buttonLabel = "Create" }) {
         { defaultValues: initialCourse || {}, }
     );
     // Stryker enable all
-    
-    // Stryker disable next-line Regex
-    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
 
-    // Stryker disable next-line all
-    const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
-    console.log(initialCourse);
+    const navigate = useNavigate();
+
+
     return (
 
         <Form onSubmit={handleSubmit(submitAction)}>
@@ -44,52 +37,50 @@ function CourseForm({ initialCourse, submitAction, buttonLabel = "Create" }) {
             <Form.Group className="mb-3" >
                 <Form.Label htmlFor="title">Title</Form.Label>
                 <Form.Control
-                    data-testid={"CourseForm-title"}
+                    data-testid="CourseForm-title"
                     id="title"
                     type="text"
                     isInvalid={Boolean(errors.title)}
                     {...register("title", {
-                        required: "Title is required.",
-                        maxLength : {
-                            value: 30,
-                            message: "Max length 30 characters"
-                        }
+                        required: "Title is required."
                     })}
                 />
                 <Form.Control.Feedback type="invalid">
-                    {errors.name?.message}
+                    {errors.title?.message}
                 </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" >
-                <Form.Label htmlFor="number">Course Number</Form.Label>
+                <Form.Label htmlFor="number">Number</Form.Label>
                 <Form.Control
-                    data-testid={"CourseForm-number"}
+                    data-testid="CourseForm-number"
                     id="number"
-                    type="text"
+                    type="number"
                     isInvalid={Boolean(errors.number)}
-                    {...register("number", {
-                        required: "Course Number is required."
-                    })}
+                    {...register("number", { 
+                        required: "Number is required.",
+                        valueAsNumber: true
+                     })}
+
                 />
                 <Form.Control.Feedback type="invalid">
-                    {errors.description?.message}
+                    {errors.number && 'Number is required.'}
                 </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" >
                 <Form.Label htmlFor="instructor">Instructor</Form.Label>
                 <Form.Control
-                    data-testid={"CourseForm-instructor"}
+                    data-testid="CourseForm-instructor"
                     id="instructor"
                     type="text"
                     isInvalid={Boolean(errors.instructor)}
-                    {...register("instructor", {
+                    {...register("instructor", { 
                         required: "Instructor is required."
                     })}
                 />
                 <Form.Control.Feedback type="invalid">
-                    {errors.description?.message}
+                    {errors.instructor && 'Instructor is required.'}
                 </Form.Control.Feedback>
             </Form.Group>
 
--- frontend/src/tests/components/Courses/CourseForm.test.js
+++ frontend/src/tests/components/Courses/CourseForm.test.js
@@ -1,6 +1,6 @@
 import { render, waitFor, fireEvent } from "@testing-library/react";
 import CourseForm from "main/components/Courses/CourseForm";
-import {courseFixtures} from "fixtures/courseFixtures";
+import { courseFixtures } from "fixtures/courseFixtures";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const mockedNavigate = jest.fn();
@@ -23,21 +23,189 @@ describe("CourseForm tests", () => {
     });
 
     test("renders correctly when passing in a Course", async () => {
+
+        const { getByText, getByTestId, findByTestId } = render(
+            <Router  >
+                <CourseForm initialCourse={courseFixtures.oneCourse}  />
+            </Router>
+        );
+        await findByTestId(/CourseForm-id/);
+        expect(getByText(/Id/)).toBeInTheDocument();
+        expect(getByTestId(/CourseForm-id/)).toHaveValue("1");
+    });
+
+    //////
+    test("Correct Error messsages on bad input", async () => {
+
+        const { getByTestId, getByText, findByTestId, findByText } = render(
+            <Router  >
+                <CourseForm />
+            </Router>
+        );
+        await findByTestId("CourseForm-title");
+        const titleField = getByTestId("CourseForm-title");
+        const numberField = getByTestId("CourseForm-number");
+        const submitButton = getByTestId("CourseForm-submit");
+
+        fireEvent.change(titleField, { target: { value: 'bad-input' } });
+        fireEvent.change(numberField, { target: { value: 'bad-input' } });
+        fireEvent.click(submitButton);
+        // await findByText(/title must be in the format YYYYQ/);
+        // expect(getByText(/localDateTime must be in ISO format/)).toBeInTheDocument();
+    });
+
+    test("Correct Error messsages on missing input", async () => {
+
+        const { getByTestId, getByText, findByTestId, findByText } = render(
+            <Router  >
+                <CourseForm />
+            </Router>
+        );
+        await findByTestId("CourseForm-submit");
+        const submitButton = getByTestId("CourseForm-submit");
+
+        fireEvent.click(submitButton);
+
+        await findByText(/Title is required./);
+        expect(getByText(/Number is required./)).toBeInTheDocument();
+        expect(getByText(/Instructor is required./)).toBeInTheDocument();
+
+    });
+
+    test("No Error messsages on good input", async () => {
+
+        const mockSubmitAction = jest.fn();
+
+
+        const { getByTestId, queryByText, findByTestId } = render(
+            <Router  >
+                <CourseForm submitAction={mockSubmitAction} />
+            </Router>
+        );
+        await findByTestId("CourseForm-title");
+
+        const titleField = getByTestId("CourseForm-title");
+        const numberField = getByTestId("CourseForm-number");
+        const instructorField = getByTestId("CourseForm-instructor");
+        const submitButton = getByTestId("CourseForm-submit");
+
+        fireEvent.change(titleField, { target: { value: 'The Habit' } });
+        fireEvent.change(numberField, { target: { value: 3} });
+        fireEvent.change(instructorField, { target: { value: "Phil Conrad" } });
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
+
+
+    });
+
+
+    test("that navigate(-1) is called when Cancel is clicked", async () => {
+
+        const { getByTestId, findByTestId } = render(
+            <Router  >
+                <CourseForm />
+            </Router>
+        );
+        await findByTestId("CourseForm-cancel");
+        const cancelButton = getByTestId("CourseForm-cancel");
+
+        fireEvent.click(cancelButton);
+        
+        await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+
+    });
+});
+
+
+/*
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
+import CourseForm from "main/components/Courses/CourseForm";
+import {courseFixtures} from "fixtures/courseFixtures";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate
+}));
+
+describe("CourseForm tests", () => {
+    const queryClient = new QueryClient();
+
+    const expectedHeaders = ["Title", "Number", "Instructor"];
+    const testId = "CourseForm";
+    test("renders correctly", async () => {
+
+        const { getByText, findByText } = render(
+            <Router  >
+                <CourseForm />
+            </Router>
+        );
+        await findByText(/Title/);
+        await findByText(/Create/);
+    });
+
+    test("renders correctly when passing in a Course", async () => {
         //console.log(courseFixtures.oneCourse);
         const { getByText, getByTestId, findByTestId } = render(
             <Router  >
                 <CourseForm initialCourse={courseFixtures.oneCourse} />
             </Router>
         );
+        expectedHeaders.forEach((headerText) => {
+            const header = screen.getByText(headerText);
+            expect(header).toBeInTheDocument();
+          });
         await findByTestId(/CourseForm-id/);
         expect(getByText(/Id/)).toBeInTheDocument();
-        console.log(getByTestId(/CourseForm-title/));
+        console.log(getByTestId(/CourseForm-id/));
         expect(getByTestId(/CourseForm-id/)).toHaveValue("1");
     });
+    test("renders correctly when passing in initialContents", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <CourseForm initialContents={courseFixtures.oneCourse} />
+                </Router>
+            </QueryClientProvider>
+        );
 
+        expect(await screen.findByText(/Create/)).toBeInTheDocument();
 
+        expectedHeaders.forEach((headerText) => {
+            const header = screen.getByText(headerText);
+            expect(header).toBeInTheDocument();
+        });
+
+        expect(await screen.findByTestId(`${testId}-id`)).toBeInTheDocument();
+        expect(screen.getByText(`id`)).toBeInTheDocument();
+    });
 });
-/*
+
+
+    test("renders correctly when passing in initialCourse", async () => {
+        render(
+          <QueryClientProvider client={queryClient}>
+            <Router>
+              <CourseForm initialContents={courseFixtures.oneCourse} />
+            </Router>
+          </QueryClientProvider>
+        );
+    
+        expectedHeaders.forEach((headerText) => {
+          const header = screen.getByText(headerText);
+          expect(header).toBeInTheDocument();
+        });
+    
+        expect(await screen.findByTestId(/CourseForm-id/)).toBeInTheDocument();
+        expect(screen.getByText(`Id`)).toBeInTheDocument();
+      });
+
+
+
+
 describe("CourseForm tests", () => {
     const queryClient = new QueryClient();
 
