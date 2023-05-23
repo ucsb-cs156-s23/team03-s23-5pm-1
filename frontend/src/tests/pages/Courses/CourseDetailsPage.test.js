86b48266e424d8c58bc98ed5537d6a49f5e8600d
James Ellenstein <59277482+JamesEllenstein@users.noreply.github.com>
2023-05-23 05:00:27 +0000

    testing almost done for detailspage

--- frontend/src/tests/pages/Courses/CourseDetailsPage.test.js
+++ frontend/src/tests/pages/Courses/CourseDetailsPage.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import {fireEvent, render, waitFor } from "@testing-library/react";
 import CourseDetailsPage from "main/pages/Courses/CourseDetailsPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -6,15 +6,33 @@ import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
+import mockConsole from "jest-mock-console";
+
+const mockToast = jest.fn();
+jest.mock("react-toastify", () => {
+    const originalModule = jest.requireActual("react-toastify");
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x),
+    };
+});
 
 const mockNavigate = jest.fn();
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useParams: () => ({
-        id: 3
-    }),
-    useNavigate: () => mockNavigate
-}));
+jest.mock("react-router-dom", () => {
+    const originalModule = jest.requireActual("react-router-dom");
+    return {
+        __esModule: true,
+        ...originalModule,
+        useParams: () => ({
+            id: 1,
+        }),
+        Navigate: (x) => {
+            mockNavigate(x);
+            return null;
+        },
+    };
+});
 /*
 jest.mock('main/utils/courseUtilities', () => {
     return {
@@ -35,6 +53,164 @@ jest.mock('main/utils/courseUtilities', () => {
 });
 */
 describe("CourseDetailsPage tests", () => {
+    const axiosMock = new AxiosMockAdapter(axios);
+
+    const testId = "CourseTable";
+
+    const setupUserOnly = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock
+            .onGet("/api/currentUser")
+            .reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock
+            .onGet("/api/systemInfo")
+            .reply(200, systemInfoFixtures.showingNeither);
+    };
+
+    const setupAdminUser = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock
+            .onGet("/api/currentUser")
+            .reply(200, apiCurrentUserFixtures.adminUser);
+        axiosMock
+            .onGet("/api/systemInfo")
+            .reply(200, systemInfoFixtures.showingNeither);
+    };
+    test("renders without crashing for regular user", () => {
+        setupUserOnly();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/course").reply(200, []);
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <CourseDetailsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+    test("renders without crashing for admin user", () => {
+        setupAdminUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/course").reply(200, []);
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <CourseDetailsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+    test("renders one course without crashing for regular user", async () => {
+        setupUserOnly();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/course", { params: { id: 1 } }).reply(200, {
+            id: 1,
+            title: "Name1",
+            number: 123,
+            instructor: "Phil",
+        });
+
+        const { getByTestId, queryByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <CourseDetailsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => {
+            expect(
+                getByTestId(`${testId}-cell-row-0-col-id`)
+            ).toHaveTextContent("1");
+        });
+        expect(getByTestId(`${testId}-cell-row-0-col-title`)).toHaveTextContent(
+            "Name1"
+        );
+        expect(
+            getByTestId(`${testId}-cell-row-0-col-number`)
+        ).toHaveTextContent("123");
+        expect(
+            getByTestId(`${testId}-cell-row-0-col-instructor`)
+        ).toHaveTextContent("Phil");
+        expect(
+            queryByTestId(`${testId}-cell-row-0-col-Delete`)
+        ).not.toBeInTheDocument();
+    });
+
+    test("renders one course without crashing for admin user", async () => {
+        setupAdminUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/course", { params: { id: 1 } }).reply(200, {
+            id: 1,
+            title: "Name1",
+            number: 123,
+            instructor: "Phil",
+        });
+
+        const { getByTestId, queryByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <CourseDetailsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => {
+            expect(
+                getByTestId(`${testId}-cell-row-0-col-id`)
+            ).toHaveTextContent("1");
+        });
+        expect(getByTestId(`${testId}-cell-row-0-col-title`)).toHaveTextContent(
+            "Name1"
+        );
+        expect(
+            getByTestId(`${testId}-cell-row-0-col-number`)
+        ).toHaveTextContent("123");
+        expect(
+            getByTestId(`${testId}-cell-row-0-col-instructor`)
+        ).toHaveTextContent("Phil");
+        expect(
+            queryByTestId(`${testId}-cell-row-0-col-Delete`)
+        ).not.toBeInTheDocument();
+    });
+
+    test("renders empty table when backend unavailable, user only", async () => {
+        setupUserOnly();
+
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/course").timeout();
+
+        const restoreConsole = mockConsole();
+
+        const { queryByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <CourseDetailsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => {
+            expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1);
+        });
+
+        const errorMessage = console.error.mock.calls[0][0];
+        expect(errorMessage).toMatch(
+            "Error communicating with backend via GET on /api/course"
+        );
+        restoreConsole();
+
+        /*expect(
+            queryByTestId(`${testId}-cell-row-0-col-id`)
+        ).not.toBeInTheDocument(); */
+    });
+/*
     const axiosMock =new AxiosMockAdapter(axios);
     axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
     axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither); 
@@ -48,6 +224,7 @@ describe("CourseDetailsPage tests", () => {
             </QueryClientProvider>
         );
     });
+    */
 /*
     test("loads the correct fields, and no buttons", async () => {
         render(
